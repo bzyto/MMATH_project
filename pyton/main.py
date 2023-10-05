1ed36cb93b5f685ae1119bd164340ca0a1fbdfae
bzyto <borys.zyto@gmail.com>
2023-10-05 13:45:22 +0100

    Introduced element matrices

--- pyton/main.py
+++ pyton/main.py
@@ -16,16 +16,19 @@ u = 0 on the boundary
 ### Plot results
 class Triangle:
     def __init__(self, vertices, VertexNumbers, GlobalNumber = 0):
-        self.x_0 = vertices[0]
-        self.x_1 = vertices[1]
-        self.x_2 = vertices[2]
+        self.x_0 = np.array(vertices[0])
+        self.x_1 = np.array(vertices[1])
+        self.x_2 = np.array(vertices[2])
         self.GlobalNumber = GlobalNumber
         self.VertexNumbers = VertexNumbers
+        self.bvector = [self.x_1[1]- self.x_2[1], self.x_2[1]- self.x_0[1], self.x_0[1]- self.x_1[1]] #useful later
+        self.cvector = [self.x_2[0]- self.x_1[0], self.x_0[0]- self.x_2[0], self.x_1[0]- self.x_0[0]]
     def area(self):
         return 0.5 * np.abs(np.cross(self.x_1 - self.x_0, self.x_2 - self.x_0))
     def __str__(self):
         return "Triangle with vertices \n" + str(self.x_0) + "\n" + str(self.x_1) + "\n" + str(self.x_2) + "\n And global number " + str(self.GlobalNumber)
     def COM(self):
+        ##centre of mass
         x_coord = (1/3)*(self.x_0[0]+self.x_1[0]+self.x_2[0])
         y_coord = (1/3)*(self.x_0[1]+self.x_1[1]+self.x_2[1])
         return [x_coord, y_coord]
@@ -40,7 +43,7 @@ class Mesh:
         self.vertices = vertices
         self.triangles = triangles
     def __str__(self):
-        return "Mesh with " + str(len(self.vertices)**2) + " vertices and " + str(len(self.triangles)) + " triangles"
+        return "Mesh with " + str(len(self.vertices)) + " vertices and " + str(len(self.triangles)) + " triangles"
     def plot(self, numbering = False):
         for triangle in self.triangles:
             plt.plot([triangle.x_0[0], triangle.x_1[0]], [triangle.x_0[1], triangle.x_1[1]], 'k-')
@@ -48,11 +51,9 @@ class Mesh:
             plt.plot([triangle.x_2[0], triangle.x_0[0]], [triangle.x_2[1], triangle.x_0[1]], 'k-')
             if numbering:
                 plt.text(triangle.COM()[0], triangle.COM()[1], str(triangle.GlobalNumber), color = 'red')
-                plt.text(triangle.x_0[0], triangle.x_0[1], str(triangle.VertexNumbers[0]))
-                plt.text(triangle.x_1[0], triangle.x_1[1], str(triangle.VertexNumbers[1]))
-                plt.text(triangle.x_2[0], triangle.x_2[1], str(triangle.VertexNumbers[2]))
-
-
+                # plt.text(triangle.x_0[0], triangle.x_0[1], str(triangle.VertexNumbers[0]))
+                # plt.text(triangle.x_1[0], triangle.x_1[1], str(triangle.VertexNumbers[1]))
+                # plt.text(triangle.x_2[0], triangle.x_2[1], str(triangle.VertexNumbers[2]))
         plt.show()
     def WhatsInside(self):
         for triangle in self.triangles:
@@ -63,7 +64,32 @@ class Mesh:
             for j in range(3):
                 CMatrix[i][j] = self.triangles[i].VertexNumbers[j]
         return CMatrix
-
+class PoissonSolver:
+    def __init__(self, mesh, bc):
+        self.mesh = mesh
+        self.bc = bc
+    def ElementIntegrationLHS(self):
+        n_triangles = len(self.mesh.triangles)
+        IntegrationMatrix = np.zeros((n_triangles, 3, 3))
+        for i in range(n_triangles):
+            for j in range(3):
+                for k in range(3):
+                    IntegrationMatrix[i][j][k] = 0.25 * self.mesh.triangles[i].area()*(self.mesh.triangles[i].bvector[j]*self.mesh.triangles[i].bvector[k]+
+                                                                                       self.mesh.triangles[i].cvector[j]*self.mesh.triangles[i].cvector[k]) # as seen in MTx6052FiniteelementI.pdf page 13
+        return IntegrationMatrix
+    def ElementIntegrationRHS(self):
+        n_triangles = len(self.mesh.triangles)
+        RHSVector = np.zeros(3, n_triangles)
+        for i in range(3):
+            for j in range(n_triangles):
+                RHSVector[i][j] = self.mesh.triangles[j].area()/3
+        return RHSVector
+    def Assembly(self):
+        pass
+    def solve(self):
+        pass
+    def plot(self):
+        pass
 def generateMesh_UnitSquare(h = 0.2):
     x = y = np.linspace(0, 1, int(1/h)+1)
     x_grid, y_grid = np.meshgrid(x, y)
@@ -79,22 +105,23 @@ def generateMesh_UnitSquare(h = 0.2):
     loopcounter = 0
     for i in range(len(x)-1):
         for j in range(len(y)-1):
+            ## we take care both of the "real" coordinates of the vertices and their numbering
+            ## as a result we are able to create both mesh and the related connectivity matrix
             v1 = vertices[i][j]
             v2 = vertices[i+1][j]
             v3 = vertices[i+1][j+1]
             v4 = vertices[i][j+1]
-            triangles.append(Triangle([v1.coordinates, v2.coordinates, v4.coordinates], [v1.global_number, v2.global_number, v4.global_number], loopcounter))
-            triangles.append(Triangle([v4.coordinates, v2.coordinates, v3.coordinates], [v4.global_number, v2.global_number, v3.global_number], loopcounter+1))
+            triangles.append(Triangle([v1.coordinates, v2.coordinates, v4.coordinates], [v1.global_number, v2.global_number, v4.global_number], loopcounter)) ##lower triangle
+            triangles.append(Triangle([v4.coordinates, v2.coordinates, v3.coordinates], [v4.global_number, v2.global_number, v3.global_number], loopcounter+1)) ## upper triangle
             loopcounter+=2
-            
+    vertices = vertices.reshape((len(x)*len(y)))
     return Mesh(vertices, triangles)
 def main():
     start = time.time()
-    mesh = generateMesh_UnitSquare(0.25)
+    mesh = generateMesh_UnitSquare(0.5)
     print(f"Took {time.time()- start} seconds")
-    mesh.plot(numbering=True)
-    print(mesh.ConnectivityMatrix())
-    #print(mesh)
+    solution = PoissonSolver(mesh, 1)
+    solution.ElementIntegrationLHS()
 
 
 if __name__ == "__main__":
--- pyton/test_main.py
+++ pyton/test_main.py
@@ -1,3 +1,13 @@
 from main import *
 def test_area():
-    assert Triangle(np.array([[0,0],[1,0],[0,1]])).area() == 0.5
+    assert Triangle(np.array([[0,0],[1,0],[0,1]]), [0, 1, 2]).area() == 0.5
+def test_UnitMeshHalf_NumberVertices():
+    mesh = generateMesh_UnitSquare(0.5)
+    assert len(mesh.vertices)==9
+def test_UnitMeshHalf_NumberTriangles():
+    mesh = generateMesh_UnitSquare(0.5)
+    assert len(mesh.triangles) == 8
+def test_ElementIntegrationMatrix():
+    pass
+def test_ElementIntegrationRHS():
+    pass
